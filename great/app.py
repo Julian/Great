da428d38d507f0b76c4a773d928b372cc26fcfb3
Julian Berman <Julian@GrayVines.com>
2013-06-14 09:51:40 -0400

    Parametrize config.

--- great/app.py
+++ great/app.py
@@ -1,18 +1,32 @@
+import os
 import uuid
 
 from flask import Flask
+from flask.config import Config
 from flask.ext.admin import Admin
 from flask_debugtoolbar import DebugToolbarExtension
 
 from great.models.core import db
+import great
 
 
-def create_app(config=()):
+def create_config(root_path=None, *args, **kwargs):
+    if root_path is None:
+        root_path = os.path.abspath(os.path.dirname(great.__file__))
+
+    config = Config(root_path, *args, **kwargs)
+    config.from_object("great.config")
+    config.from_envvar("GREAT_CONFIG")
+    config.from_object(config)
+    return config
+
+
+def create_app(config=None):
+    if config is None:
+        config = create_config()
+
     app = Flask(__name__)
-
-    app.config.from_object("great.config")
-    app.config.from_envvar("GREAT_CONFIG")
-    app.config.from_object(config)
+    app.config.update(config)
 
     db.init_app(app)
 
