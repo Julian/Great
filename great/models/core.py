4fefa123eaf42a9b2540ecaff78b69269cc18432
Julian Berman <Julian@GrayVines.com>
2014-09-01 23:07:18 -0400

    Alchimia bro.

--- great/models/core.py
+++ great/models/core.py
@@ -1,62 +1,57 @@
-import datetime
+from datetime import datetime
 
-from flask.ext.sqlalchemy import SQLAlchemy
-from sqlalchemy import sql
-from sqlalchemy.orm import deferred, validates
-from sqlalchemy.ext.declarative import AbstractConcreteBase, declared_attr
-
-
-db = SQLAlchemy()
+from sqlalchemy import (
+    Boolean, Column, DateTime, Integer, Unicode, UnicodeText,
+    CheckConstraint, MetaData, Table, sql,
+)
 
 
-class ModelMixin(object):
-    id = db.Column(db.Integer, primary_key=True)
-
-    name = db.Column(db.Unicode(256))
-    rating = db.Column(db.Integer)
-
-    created_at = db.Column(db.DateTime, default=datetime.datetime.now)
-    modified_at = db.Column(db.DateTime, onupdate=datetime.datetime.now)
-
-    @declared_attr
-    def comments(cls):
-        return deferred(db.Column(db.UnicodeText))
-
-    @validates("rating")
-    def validate_rating(self, name, rating):
-        if rating is not None and not 0 <= rating <= 10:
-            raise ValueError("%r is not between 0 and 10." % (rating,))
-        return rating
-
-    def __repr__(self):
-        message = "<{0.__class__.__name__} id={0.id} name={0.name!r}>"
-        return message.format(self)
-
-    @classmethod
-    def get_or_create(cls, session, **kwargs):
-        model = session.query(cls).filter_by(**kwargs).first()
-        if model is not None:
-            return model
-        return cls(**kwargs)
+METADATA = MetaData()
 
 
-class Model(ModelMixin, AbstractConcreteBase, db.Model):
-    pass
+def table(name, *args, **kwargs):
+    with_dates = kwargs.pop("with_dates", False)
+    if with_dates:
+        now = datetime.now
+        args += (
+            Column("created_at", DateTime, default=now),
+            Column("modified_at", DateTime, default=now, onupdate=now),
+        )
+    return Table(name, METADATA, *(table_columns() + args), **kwargs)
 
 
-class Media(ModelMixin, AbstractConcreteBase, db.Model):
-    genre = db.Column(db.Unicode(64))
-    year = db.Column(db.Integer)
+def table_columns():
+    return (
+        Column("id", Integer, primary_key=True),
+        Column("name", Unicode(256)),
 
-    pinned = db.Column(
-        db.Boolean,
-        default=False,
-        nullable=False,
-        server_default=sql.expression.false(),
+        Column(
+            "rating",
+            Integer,
+            CheckConstraint("rating >= 0 AND rating <= 10"),
+        ),
+
+        Column(
+            "pinned",
+            Boolean,
+            default=False,
+            nullable=False,
+            server_default=sql.expression.false(),
+        ),
+
+        Column("comments", UnicodeText),
     )
 
-    play_count = db.Column(db.Integer, default=0, nullable=False)
-    played_at = db.Column(db.DateTime)
 
-    skip_count = db.Column(db.Integer, default=0, nullable=False)
-    skipped_at = db.Column(db.DateTime)
+def media_table(name, *args, **kwargs):
+    return Table(name, METADATA, *(media_table_columns() + args), **kwargs)
+
+
+def media_table_columns():
+    return table_columns() + (
+        Column("play_count", Integer, default=0, nullable=False),
+        Column("played_at", DateTime),
+
+        Column("skip_count", Integer, default=0, nullable=False),
+        Column("skipped_at", DateTime),
+    )
