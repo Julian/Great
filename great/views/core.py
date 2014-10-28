f2b6e8559586844a3d60221d5cd2c031030c5549
Julian Berman <Julian@GrayVines.com>
2014-10-27 20:47:13 -0400

    Read config from a file.

--- great/views/core.py
+++ great/views/core.py
@@ -1,12 +1,23 @@
+import configparser
+
+from appdirs import user_config_dir
 from minion import Application
 from sqlalchemy import create_engine
+from twisted.python.filepath import FilePath
 
 from great.views import music
 
 
+CONFIG_HOME = FilePath(user_config_dir("Great"))
+
+
 def create_app():
+    config = configparser.ConfigParser()
+    config.read(CONFIG_HOME.child("config.ini").path)
+
     app = Application()
-    engine = create_engine("sqlite:///great.db")
-    app.bin.provides("db")(engine.connect)
+    app.bin.provides("db")(create_engine(config["db"]["url"]).connect)
+
     music.init_app(app)
+
     return app
