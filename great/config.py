fe603c2ff24285df0ab0d63dabd5dcf9f73a1081
Julian Berman <Julian@GrayVines.com>
2014-11-16 12:53:05 -0500

    Flask cruft

--- great/config.py
+++
@@ -1,7 +0,0 @@
-AUTHOR = "Julian Berman"
-DESCRIPTION = "Great rates things that do not rate themselves."
-PROJECT_NAME = "Great"
-
-DEBUG = False
-
-STATIC_URL = "/static"
--- great/resources.py
+++
@@ -1,4 +0,0 @@
-from flask.ext.restful import Api
-
-
-api = Api()
