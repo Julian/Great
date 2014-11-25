2d1940b469f8dee60624267da244fda54fb10c92
Julian Berman <Julian@GrayVines.com>
2014-11-25 08:10:53 -0500

    Sort stuff.

--- great/static/js/models/artist.js
+++ great/static/js/models/artist.js
@@ -8,5 +8,6 @@ great.Artist = Backbone.Model.extend({
 
 great.ArtistsCollection = Backbone.Collection.extend({
     model: great.Artist,
+    comparator: "name",
     url: "/great/music/artists/",
 });
