84cf58dccbc14aec8f68a498e732a7703c6f8104
Julian Berman <Julian@GrayVines.com>
2018-01-28 19:35:04 -0500

    Stub data.

--- great/static/js/app.js
+++ great/static/js/app.js
@@ -1,69 +1,11 @@
 "use strict";
-
-
-var great = {
-    views: {},
-    models: {},
-
-    loadTemplates: function (views) {
-        var requests = [];
-
-        $.each(views, function(index, view) {
-            var templateURL = "/templates/" + view + ".html"
-            var promise = Promise.resolve($.get(templateURL)).then(
-                function (data) {
-                    great[view].prototype.template = _.template(data);
-                }
-            );
-            requests.push(promise);
-        });
-
-        return Promise.all(requests);
-    },
-};
-
-
-great.Router = Backbone.Router.extend({
-
-    routes: {
-        "": "home",
-    },
-
-    initialize: function() {
-        this.$content = $("#content");
-    },
-
-    home: function () {
-        great.artists = new great.ArtistsCollection();
-
-        var self = this;
-        Promise.resolve(
-            great.artists.fetch(
-                {dataType: "json", data: {fields: "mbid,rating"}}
-            )
-        ).then(
-            function () {
-                great.artistsListView = new great.ArtistsListView(
-                    {model: great.artists}
-                );
-                great.artistsListView.render()
-                self.$content.html(great.artistsListView.el);
-            }
-        );
-    }
-
-});
-
-
-$(document).on("ready", function () {
-    var views = ["ArtistsListView", "ArtistView"];
-    great.loadTemplates(views).then(
-        function () {
-            great.router = new great.Router();
-            Backbone.history.start();
-        },
-        function (error) { 
-            console.error(error);
-        }
-    )
+const vm = new Vue({
+  el: '#app',
+  data: {
+    albums: [
+      {name: "Something", artist: {name: "Someone"}},
+      {name: "Another Thing", artist: {name: "Same One"}},
+      {name: "A Third Thing", artist: {name: "A Different One"}},
+    ],
+  },
 });
