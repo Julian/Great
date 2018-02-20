7513eec7c2df2fe22b8a26bb9dbcfa696d30e574
Julian Berman <Julian@GrayVines.com>
2018-02-20 09:05:49 -0500

    And stub out Tracked

---
+++ frontend/src/components/Tracked.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="uk-container" id="app">
+    <table class="uk-table">
+      <caption>Tracked</caption>
+      <tbody v-for="artist in artists" :key="artist.id">
+        <tr>
+          <td>{{ artist.name }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tracked',
+  data() {
+    return {
+      artists: [
+        { name: 'Someone' },
+        { name: 'Someone Else' },
+        { name: 'Someone Also Else' },
+      ],
+    };
+  },
+};
+</script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+
 import Radar from '@/components/Radar';
+import Tracked from '@/components/Tracked';
 
 Vue.use(Router);
 
@@ -10,6 +12,11 @@ export default new Router({
       path: '/radar',
       name: 'Radar',
       component: Radar,
+    },
+    {
+      path: '/tracked',
+      name: 'Tracked',
+      component: Tracked,
     },
   ],
 });
