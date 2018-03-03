28bddb3a10cb019271506acf722aaadd97215030
Julian Berman <Julian@GrayVines.com>
2018-03-03 17:45:15 -0500

    Go away.

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -2,8 +2,7 @@
   <nav class="uk-navbar-container" uk-navbar>
     <div class="uk-navbar-center">
       <ul class="uk-navbar-nav">
-        <li><router-link to="/">Home</router-link></li>
-        <li><router-link to="/library">Library</router-link></li>
+        <li><router-link to="/">Library</router-link></li>
         <li><router-link to="/radar">Radar</router-link></li>
         <li><router-link to="/tracked">Tracked</router-link></li>
       </ul>
