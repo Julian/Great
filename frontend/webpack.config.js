b745efaaba9e0880eabbaa7e830c0c48dd17ca96
Julian Berman <Julian@GrayVines.com>
2019-02-16 16:30:31 -0500

    And now enable friendly errors, though not sure I like it.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
+
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
 
 function resolve (dir) {
   return path.join(__dirname, dir)
@@ -35,6 +37,7 @@ module.exports = {
         ]
     },
     plugins: [
-        new VueLoaderPlugin()
+        new FriendlyErrorsWebpackPlugin(),
+        new VueLoaderPlugin(),
     ]
 }
