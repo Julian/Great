3e6e18d39c8c73519891f190a4baf1957049531f
Julian Berman <Julian@GrayVines.com>
2019-02-19 21:21:08 -0500

    First pass at starting to get the frontend stuff into setup.py.
    This is total nonsense, but will iterate on top of it.

--- setup.py
+++ setup.py
@@ -1,7 +1,44 @@
-from setuptools import find_packages, setup
+import distutils.command.build
+import os
+import setuptools
+
 from great import __url__
-setup(
+
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+FRONTEND = os.path.join(HERE, "frontend")
+STATIC = os.path.join(HERE, "great", "static")
+
+
+class BuildFrontend(setuptools.Command):
+
+    description = "build the Great frontend"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def run(self):
+        self.spawn(
+            [
+                "npm",
+                "--prefix", FRONTEND,
+                "run-script", "build:dev",
+                "--",
+                "--output-public-path", "/static/",
+                "--output-path", STATIC,
+            ],
+        )
+
+    def finalize_options(self):
+        pass
+
+
+setuptools.setup(
     url=__url__,
-    packages=find_packages() + ["twisted.plugins"],
+    packages=setuptools.find_packages() + ["twisted.plugins"],
     use_scm_version=True,
+    cmdclass={
+        "build_frontend": BuildFrontend,
+    },
 )
