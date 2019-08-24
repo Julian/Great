90807201a158b144177431a80b074dd7aabff335
Julian Berman <Julian@GrayVines.com>
2019-08-24 08:52:34 +0200

    Slightly better canonicalizing.

--- automanual/add_artists.py
+++ automanual/add_artists.py
@@ -11,20 +11,29 @@ from titlecase import titlecase
 e = engine_from_config()
 
 
+def canonicalize(artist):
+    if artist.isupper():
+        return artist
+    return titlecase(artist)
+
+
 def exists(artist):
     return e.execute(
         sql.exists(
-            sql.select([music.artists]).where(music.artists.c.name == artist),
+            sql.select([music.artists]).where(
+                music.artists.c.name.ilike(artist),
+            ),
         ).select(),
     ).scalar()
 
 
 with open("/dev/tty") as tty:
     for line in sys.stdin:
-        artist = titlecase(line[:-1].decode("utf-8"))
+        artist = canonicalize(line[:-1].decode("utf-8"))
         if not exists(artist):
-            copy(artist)
+            copy(line[:-1].decode("utf-8"))
             print repr(artist)
             add = tty.readline().strip().decode("utf-8") or artist
-            print "Adding:", repr(add)
-            e.execute(music.artists.insert().values(name=add))
+            if add == artist or not exists(add):
+                print "Adding:", repr(add)
+                e.execute(music.artists.insert().values(name=add))
